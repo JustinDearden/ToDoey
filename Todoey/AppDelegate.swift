cc7db42d16518e43f883058df0378bb34805fe2a
Justin Dearden <justin@Justins-MacBook-Pro.local>
2019-01-02 17:01:42 -0500

    Uploading

---
+++ Todoey/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  Todoey
+//
+//  Created by Justin Dearden on 12/18/2018.
+//  Copyright © 2018 Justin Dearden. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+
+        
+        do {
+            _ = try Realm()
+        } catch {
+            print("Error initialising new realm, \(error)")
+        }
+        
+        
+        return true
+    }
+
+
+    
+
+
+
+}
+
